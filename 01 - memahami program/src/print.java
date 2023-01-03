a6de49213d303acc20dc8836011abad00602be21
Glenn <121842204+GlenAguh@users.noreply.github.com>
2023-01-03 20:25:39 +0800

    Menambahkan memahami program

---
+++ 01 - memahami program/src/print.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.CountDownLatch;
+
+public class print {
+    public static void main(String[] args) {
+        System.out.println("I Am ");
+        System.out.println("Still Youu");
+
+        System.out.println("");
+        System.out.println("");
+
+        System.out.print("Aku");
+        System.out.print("masih menginginkan mu");
+
+        System.out.println("");
+        System.out.println("");
+
+        System.out.printf("Glenn \n %d",4);
+    }
+}
